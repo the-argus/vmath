471e19b4007eb3eeede54abe681d52cf2b4d1627
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-11-08 01:52:43 -0500

    format mat4x4 pitchyawroll

--- include/vmath/mat4x4_f32.h
+++ include/vmath/mat4x4_f32.h
@@ -303,92 +303,128 @@ VMATH_INLINE vm_mat4x4f_t
 vm_load_rotation_pitch_yaw_rollv_mat4x4f(vm_v3f_t angles)
 {
 #if defined(VMATH_SSE41_ENABLE)
-    vm_v3f_t sin_angles; 
-    vm_v3f_t cos_angles; 
-    vm_sin_cos_v3f(&sin_angles, &cos_angles, angles);
+	vm_v3f_t sin_angles;
+	vm_v3f_t cos_angles;
+	vm_sin_cos_v3f(&sin_angles, &cos_angles, angles);
 
-    // TODO: optimize these full shuffles, some cases can dont need full shuffle and select
-    __m128 p0;
-    // select: { cos.x, sin.z, cos.z, cos.x }
-    // XMVECTOR P0 = XMVectorPermute<XM_PERMUTE_1X, XM_PERMUTE_0Z, XM_PERMUTE_1Z, XM_PERMUTE_1X>(SinAngles, CosAngles);
-    // NOLINTNEXTLINE
-    VMATH_SSE_FULL_SHUFFLE(p0, sin_angles, cos_angles, _MM_SHUFFLE(0, 2, 2, 0), vm_v4_selectmask_1011)
+	// TODO: optimize these full shuffles, some cases can dont need full shuffle
+	// and select
+	__m128 p0;
+	// select: { cos.x, sin.z, cos.z, cos.x }
+	// XMVECTOR P0 = XMVectorPermute<XM_PERMUTE_1X, XM_PERMUTE_0Z,
+	// XM_PERMUTE_1Z, XM_PERMUTE_1X>(SinAngles, CosAngles); NOLINTNEXTLINE
+	VMATH_SSE_FULL_SHUFFLE(p0, sin_angles, cos_angles, _MM_SHUFFLE(0, 2, 2, 0),
+						   vm_v4_selectmask_1011)
 
-    __m128 y0;
-    // select: { sin.y, cos.x, cos.x, cos.y }
-    // XMVECTOR Y0 = XMVectorPermute<XM_PERMUTE_0Y, XM_PERMUTE_1X, XM_PERMUTE_1X, XM_PERMUTE_1Y>(SinAngles, CosAngles);
-    // NOLINTNEXTLINE
-    VMATH_SSE_FULL_SHUFFLE(y0, sin_angles, cos_angles, _MM_SHUFFLE(1, 0, 0, 1), vm_v4_selectmask_0111)
+	__m128 y0;
+	// select: { sin.y, cos.x, cos.x, cos.y }
+	// XMVECTOR Y0 = XMVectorPermute<XM_PERMUTE_0Y, XM_PERMUTE_1X,
+	// XM_PERMUTE_1X, XM_PERMUTE_1Y>(SinAngles, CosAngles); NOLINTNEXTLINE
+	VMATH_SSE_FULL_SHUFFLE(y0, sin_angles, cos_angles, _MM_SHUFFLE(1, 0, 0, 1),
+						   vm_v4_selectmask_0111)
 
-    __m128 p1;
-    // select: { cos.z, sin.z, cos.z, sin.z }
-    // XMVECTOR P1 = XMVectorPermute<XM_PERMUTE_1Z, XM_PERMUTE_0Z, XM_PERMUTE_1Z, XM_PERMUTE_0Z>(SinAngles, CosAngles);
-    // NOLINTNEXTLINE
-    VMATH_SSE_FULL_SHUFFLE(p1, sin_angles, cos_angles, _MM_SHUFFLE(2, 2, 2, 2), vm_v4_selectmask_1010)
+	__m128 p1;
+	// select: { cos.z, sin.z, cos.z, sin.z }
+	// XMVECTOR P1 = XMVectorPermute<XM_PERMUTE_1Z, XM_PERMUTE_0Z,
+	// XM_PERMUTE_1Z, XM_PERMUTE_0Z>(SinAngles, CosAngles); NOLINTNEXTLINE
+	VMATH_SSE_FULL_SHUFFLE(p1, sin_angles, cos_angles, _MM_SHUFFLE(2, 2, 2, 2),
+						   vm_v4_selectmask_1010)
 
-    __m128 y1;
-    // select: { cos.y, cos.y, sin.y, sin.y }
-    // XMVECTOR Y1 = XMVectorPermute<XM_PERMUTE_1Y, XM_PERMUTE_1Y, XM_PERMUTE_0Y, XM_PERMUTE_0Y>(SinAngles, CosAngles);
-    // NOLINTNEXTLINE
-    VMATH_SSE_FULL_SHUFFLE(y1, sin_angles, cos_angles, _MM_SHUFFLE(1, 1, 1, 1), vm_v4_selectmask_1100)
+	__m128 y1;
+	// select: { cos.y, cos.y, sin.y, sin.y }
+	// XMVECTOR Y1 = XMVectorPermute<XM_PERMUTE_1Y, XM_PERMUTE_1Y,
+	// XM_PERMUTE_0Y, XM_PERMUTE_0Y>(SinAngles, CosAngles); NOLINTNEXTLINE
+	VMATH_SSE_FULL_SHUFFLE(y1, sin_angles, cos_angles, _MM_SHUFFLE(1, 1, 1, 1),
+						   vm_v4_selectmask_1100)
 
-    __m128 p2;
-    // select: { sin.z, cos.z, sin.z, cos.z }
-    // XMVECTOR P2 = XMVectorPermute<XM_PERMUTE_0Z, XM_PERMUTE_1Z, XM_PERMUTE_0Z, XM_PERMUTE_1Z>(SinAngles, CosAngles);
-    // NOLINTNEXTLINE
-    VMATH_SSE_FULL_SHUFFLE(p2, sin_angles, cos_angles, _MM_SHUFFLE(2, 2, 2, 2), vm_v4_selectmask_0101)
+	__m128 p2;
+	// select: { sin.z, cos.z, sin.z, cos.z }
+	// XMVECTOR P2 = XMVectorPermute<XM_PERMUTE_0Z, XM_PERMUTE_1Z,
+	// XM_PERMUTE_0Z, XM_PERMUTE_1Z>(SinAngles, CosAngles); NOLINTNEXTLINE
+	VMATH_SSE_FULL_SHUFFLE(p2, sin_angles, cos_angles, _MM_SHUFFLE(2, 2, 2, 2),
+						   vm_v4_selectmask_0101)
 
-    __m128 p3;
-    // select: { sin.y, sin.y, cos.y, cos.y }
-    // XMVECTOR P3 = XMVectorPermute<XM_PERMUTE_0Y, XM_PERMUTE_0Y, XM_PERMUTE_1Y, XM_PERMUTE_1Y>(SinAngles, CosAngles);
-    // NOLINTNEXTLINE
-    VMATH_SSE_FULL_SHUFFLE(p3, sin_angles, cos_angles, _MM_SHUFFLE(1, 1, 1, 1), vm_v4_selectmask_0011)
+	__m128 p3;
+	// select: { sin.y, sin.y, cos.y, cos.y }
+	// XMVECTOR P3 = XMVectorPermute<XM_PERMUTE_0Y, XM_PERMUTE_0Y,
+	// XM_PERMUTE_1Y, XM_PERMUTE_1Y>(SinAngles, CosAngles); NOLINTNEXTLINE
+	VMATH_SSE_FULL_SHUFFLE(p3, sin_angles, cos_angles, _MM_SHUFFLE(1, 1, 1, 1),
+						   vm_v4_selectmask_0011)
 
-    // select { sin.x, sin.x, sin.x, sin.x }
-    // NOLINTNEXTLINE
-    const __m128 y2 = _mm_shuffle_ps(sin_angles, sin_angles, _MM_SHUFFLE(0, 0, 0, 0));
-    const __m128 ns = _mm_mul_ps(sin_angles, vm_v4_negative_one.vector_rep);
+	// select { sin.x, sin.x, sin.x, sin.x }
+	const __m128 y2 =
+		// NOLINTNEXTLINE
+		_mm_shuffle_ps(sin_angles, sin_angles, _MM_SHUFFLE(0, 0, 0, 0));
+	const __m128 ns = _mm_mul_ps(sin_angles, vm_v4_negative_one.vector_rep);
 
-    __m128 q0 = _mm_mul_ps(p0, y0);
-    __m128 q1 = _mm_mul_ps(p1, vm_v4_pitchyawroll_signs.vector_rep);
-    q1 = _mm_mul_ps(q1, y1);
-    __m128 q2 = _mm_mul_ps(p2, y2);
-    q2 = VMATH_FMADD_PS(q2, p3, q1);
+	__m128 q0 = _mm_mul_ps(p0, y0);
+	__m128 q1 = _mm_mul_ps(p1, vm_v4_pitchyawroll_signs.vector_rep);
+	q1 = _mm_mul_ps(q1, y1);
+	__m128 q2 = _mm_mul_ps(p2, y2);
+	q2 = VMATH_FMADD_PS(q2, p3, q1);
 
-    // XMVECTOR V0 = XMVectorPermute<XM_PERMUTE_1X, XM_PERMUTE_0Y, XM_PERMUTE_1Z, XM_PERMUTE_0W>(Q0, Q2);
-    const __m128 v0 = vm_select_v4f(q0, q2, vm_v4_selectmask_1010.vector_rep);
-    __m128 v1;
-    __m128 v2;
-    // XMVECTOR V1 = XMVectorPermute<XM_PERMUTE_1Y, XM_PERMUTE_0Z, XM_PERMUTE_1W, XM_PERMUTE_0W>(Q0, Q2);
-    // NOLINTNEXTLINE
-    VMATH_SSE_FULL_SHUFFLE(v1, q0, q2, _MM_SHUFFLE(1, 2, 3, 3), vm_v4_selectmask_1010)
-    // XMVECTOR V2 = XMVectorPermute<XM_PERMUTE_0X, XM_PERMUTE_1X, XM_PERMUTE_0W, XM_PERMUTE_0W>(Q0, NS);
-    // NOLINTNEXTLINE
-    VMATH_SSE_FULL_SHUFFLE(v2, q0, ns, _MM_SHUFFLE(0, 0, 3, 3), vm_v4_selectmask_0100)
+	// XMVECTOR V0 = XMVectorPermute<XM_PERMUTE_1X, XM_PERMUTE_0Y,
+	// XM_PERMUTE_1Z, XM_PERMUTE_0W>(Q0, Q2);
+	const __m128 v0 = vm_select_v4f(q0, q2, vm_v4_selectmask_1010.vector_rep);
+	__m128 v1;
+	__m128 v2;
+	// XMVECTOR V1 = XMVectorPermute<XM_PERMUTE_1Y, XM_PERMUTE_0Z,
+	// XM_PERMUTE_1W, XM_PERMUTE_0W>(Q0, Q2); NOLINTNEXTLINE
+	VMATH_SSE_FULL_SHUFFLE(v1, q0, q2, _MM_SHUFFLE(1, 2, 3, 3),
+						   vm_v4_selectmask_1010)
+	// XMVECTOR V2 = XMVectorPermute<XM_PERMUTE_0X, XM_PERMUTE_1X,
+	// XM_PERMUTE_0W, XM_PERMUTE_0W>(Q0, NS); NOLINTNEXTLINE
+	VMATH_SSE_FULL_SHUFFLE(v2, q0, ns, _MM_SHUFFLE(0, 0, 3, 3),
+						   vm_v4_selectmask_0100)
 
-    vm_mat4x4f_t matrix;
+	vm_mat4x4f_t matrix;
 
 #if defined(VMATH_AVX512_GENERIC_ENABLE)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wuninitialized"
-	matrix = _mm512_insertf32x4(matrix, vm_select_v4f(vm_v4_zero.vector_rep, v0, vm_v4_selectmask_1110.vector_rep), 0);
+	matrix = _mm512_insertf32x4(matrix,
+								vm_select_v4f(vm_v4_zero.vector_rep, v0,
+											  vm_v4_selectmask_1110.vector_rep),
+								0);
 #pragma clang diagnostic pop
-	matrix = _mm512_insertf32x4(matrix, vm_select_v4f(vm_v4_zero.vector_rep, v1, vm_v4_selectmask_1110.vector_rep), 1);
-	matrix = _mm512_insertf32x4(matrix, vm_select_v4f(vm_v4_zero.vector_rep, v2, vm_v4_selectmask_1110.vector_rep), 2);
+	matrix = _mm512_insertf32x4(matrix,
+								vm_select_v4f(vm_v4_zero.vector_rep, v1,
+											  vm_v4_selectmask_1110.vector_rep),
+								1);
+	matrix = _mm512_insertf32x4(matrix,
+								vm_select_v4f(vm_v4_zero.vector_rep, v2,
+											  vm_v4_selectmask_1110.vector_rep),
+								2);
 	matrix = _mm512_insertf32x4(matrix, vm_mat4x4_iden_row3.vector_rep, 3);
 #elif defined(VMATH_AVX256_GENERIC_ENABLE)
-	matrix.buffer[0] = _mm256_insertf128_ps(matrix.buffer[0], vm_select_v4f(vm_v4_zero.vector_rep, v0, vm_v4_selectmask_1110.vector_rep), 0);
-	matrix.buffer[0] = _mm256_insertf128_ps(matrix.buffer[0], vm_select_v4f(vm_v4_zero.vector_rep, v1, vm_v4_selectmask_1110.vector_rep), 1);
-	matrix.buffer[1] = _mm256_insertf128_ps(matrix.buffer[1], vm_select_v4f(vm_v4_zero.vector_rep, v2, vm_v4_selectmask_1110.vector_rep), 0);
-	matrix.buffer[1] = _mm256_insertf128_ps(matrix.buffer[1], vm_mat4x4_iden_row3.vector_rep, 1);
+	matrix.buffer[0] =
+		_mm256_insertf128_ps(matrix.buffer[0],
+							 vm_select_v4f(vm_v4_zero.vector_rep, v0,
+										   vm_v4_selectmask_1110.vector_rep),
+							 0);
+	matrix.buffer[0] =
+		_mm256_insertf128_ps(matrix.buffer[0],
+							 vm_select_v4f(vm_v4_zero.vector_rep, v1,
+										   vm_v4_selectmask_1110.vector_rep),
+							 1);
+	matrix.buffer[1] =
+		_mm256_insertf128_ps(matrix.buffer[1],
+							 vm_select_v4f(vm_v4_zero.vector_rep, v2,
+										   vm_v4_selectmask_1110.vector_rep),
+							 0);
+	matrix.buffer[1] = _mm256_insertf128_ps(matrix.buffer[1],
+											vm_mat4x4_iden_row3.vector_rep, 1);
 #else
-    matrix.buffer[0] = vm_select_v4f(vm_v4_zero.vector_rep, v0, vm_v4_selectmask_1110.vector_rep);
-    matrix.buffer[1] = vm_select_v4f(vm_v4_zero.vector_rep, v1, vm_v4_selectmask_1110.vector_rep);
-    matrix.buffer[2] = vm_select_v4f(vm_v4_zero.vector_rep, v2, vm_v4_selectmask_1110.vector_rep);
-    matrix.buffer[3] = vm_mat4x4_iden_row3.vector_rep;
+	matrix.buffer[0] = vm_select_v4f(vm_v4_zero.vector_rep, v0,
+									 vm_v4_selectmask_1110.vector_rep);
+	matrix.buffer[1] = vm_select_v4f(vm_v4_zero.vector_rep, v1,
+									 vm_v4_selectmask_1110.vector_rep);
+	matrix.buffer[2] = vm_select_v4f(vm_v4_zero.vector_rep, v2,
+									 vm_v4_selectmask_1110.vector_rep);
+	matrix.buffer[3] = vm_mat4x4_iden_row3.vector_rep;
 #endif
 
-    return matrix;
+	return matrix;
 
 #elif defined(VMATH_ARM_ENABLE) || defined(VMATH_ARM64_ENABLE)
 #error ARM SIMD not implemented
